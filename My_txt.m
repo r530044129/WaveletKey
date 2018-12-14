fd144aeab611dfb423dbf249cf8206ebcbbe0fc4
r530044129 <530044129@qq.com>
2018-12-15 00:48:03 +0800

    Add files via upload

---
+++ My_txt.m
@@ -0,0 +1,154 @@
+% create scales
+numOctave = 8;
+numVoices = 32;
+s0=1;
+a0=2^(1/numVoices);
+scales=s0*a0.^(0:numOctave*numVoices);
+
+% 4 coefs to 1 coefs ,72 frame to 18 frame
+for V=100:300
+    for H=200:400
+        
+%         ������x ���������.(��˹ȡ��)
+        MeanSignalCount = floor(fileCount/4);
+%         ��������ȡ��
+        MeanPixelSignal=round((maskOutPutSequence(V,H,1)+maskOutPutSequence(V,H,2)+maskOutPutSequence(V,H,3)+maskOutPutSequence(V,H,4))/4);
+        for Count = 2:MeanSignalCount
+%                 StartCount
+            u=(Count-1)*4;
+            MeanPixelSignal=[MeanPixelSignal,round((maskOutPutSequence(V,H,u)+maskOutPutSequence(V,H,u+1)+maskOutPutSequence(V,H,u+2)+maskOutPutSequence(V,H,u+3))/4)];
+        end
+        
+        for frame=1:MeanSignalCount
+        MeanOutPutSequence_1to4(V,H,frame)=MeanPixelSignal(frame);        
+        end
+    end
+end
+imshow(MeanOutPutSequence_1to4(100:300,200:400,1),[0,50])
+
+% dowmsample:3 frame to 1 frame
+for V=100:300
+    
+    for H=200:400
+        
+        singlePixelSignal=imageArray(V,H,1);
+        
+        for frame=2:fileCount
+            singlePixelSignal=[singlePixelSignal,imageArray(V,H,frame)];            
+        end
+        
+        singlePixelSignal_dowmsample = singlePixelSignal(1:3:fileCount);
+        coeffecients_dowmsample = cwt(singlePixelSignal_dowmsample,CWTScale,motherWavelet);
+
+        for frame=1:24
+        maskOutPutSequence_dowmsample(V,H,frame)=uint16(abs(coeffecients_dowmsample(frame)));
+        end        
+    end
+end
+% save maskOutPutSequence data
+DataName=strcat('Data\MOPS_',motherWavelet,'_',num2str(CWTScale),'.mat');
+save(DataName,'maskOutPutSequence');
+
+imshow(maskOutPutSequence_dowmsample(100:300,200:400,1),[0,50])
+
+plot(maskOutPutSequence_dowmsample(100,200:240,1));
+hold on;
+plot(maskOutPutSequence(100,200:240,1),':');
+
+for k=1:M*N
+    %M*N,����
+    %�к�
+    i=mod(k-1,M)+1;
+    %�к�
+    j=floor((k-1)/M)+1;
+end
+
+view(az,el)    
+
+% ������
+h = waitbar(0,'Please wait...');
+        for i=1:1000
+            % computation here %
+            str=['�Ż�������...',num2str(i/10),'%'];
+            waitbar(i/1000,h,str);
+%                 waitbar(i/10000,h);
+        end;
+% close(h)
+
+clampBottom=0;
+%White
+clampHead=15;
+alphaSequence16=(((maskOutPutSequence(:,:,40)-clampBottom))*256)/(clampHead-clampBottom);
+
+if ~exist('2Figure') 
+    mkdir('2Figure')         % �������ڣ��ڵ�ǰĿ¼�в���һ����Ŀ¼��Figure��
+end 
+
+BW=imread('testPic.tif');
+% AW=BW;
+AW=255-BW;
+% for i=1:1080
+% for j=1:1920
+% if AW(i,j)>120
+% AW(i,j)=0;
+% else
+% AW(i,j)=1;
+% end
+% end
+% end
+SE=strel('square',3);
+% SE = offsetstrel('ball',3,3);
+AW_erode=imerode(AW,SE);
+AW_dilate=imdilate(AW,SE);
+AW_erode_dilate=imdilate(AW_erode,SE);
+AW_dilate_erode=imdilate(AW_dilate,SE);
+figure(3);imshow(AW,[]);title('ԭͼ');
+figure(4);imshow(AW_erode,[]);title('��ʴ');
+figure(5);imshow(AW_dilate,[]);title('����');
+figure(6);imshow(AW_erode_dilate,[]);title('������');
+figure(7);imshow(AW_dilate_erode,[]);title('������');
+
+AW_open_minus_original=AW-AW_erode_dilate;
+figure(8);title('�������ԭͼ');imshow(AW_open_minus_original,[]);
+
+% 如果运行出现错误，matlab会自动停在出错的那行，并且保存所有相关变量
+dbstop if error
+
+array(array>1)=0;
+array(and(array>1,array<3))=0;
+
+fig = figure;
+fig.Position = get(0,'ScreenSize');
+plot(1:10);
+zoom on
+
+% Find the influenced frame by the certain scale of certain wavelet
+m=12;
+Signal = plotContainer(1:2*m);
+Signal_a=Signal;
+Signal_b=Signal;
+Signal_b(m)=100;
+
+wavelet = 'bior3.3';
+scale = 2;
+a = cwt(Signal_a,scale,wavelet); 
+b = cwt(Signal_b,scale,wavelet); 
+
+front=0;back=0;
+for i=1:m-1
+    front(a(i)~=b(i))=front+1;
+end
+for i=m+1:2*m
+    back(a(i)~=b(i))=back+1;
+end
+
+% Create video
+v=VideoWriter('Video','MPEG-4');
+v.FrameRate=24;
+open(v);
+for i=1:60
+OneMaskedPicName = strcat('MaskedPic.',num2str(2*i-1),'.png');
+OneMaskedPic = imread(OneMaskedPicName);
+writeVideo(v,OneMaskedPic);
+end
+close(v);
---
+++ OutMask.m
@@ -0,0 +1,152 @@
+%ChooseMotherWavelet
+motherWavelet='bior3.3';
+CWTScale=2;
+
+% ----------------------------------------------------------------
+
+% Path
+fileMotherPath='Test-source/';
+SourcePath='Aperture/W-F5.6';
+
+outPutDataPath=strcat('Data/',SourcePath,'/');
+if ~exist(outPutDataPath) 
+    mkdir(outPutDataPath)         
+end 
+
+filePath=strcat(fileMotherPath,SourcePath,'/');
+filePathList=dir(strcat(filePath,'*.jpg'));
+
+% Variable
+fileCount = 48;
+%image vertical and horizontal resolution
+Vres=1080;
+Hres=1920;
+imageArray=zeros(Vres,Hres,fileCount);
+%create fileCount room
+maskOutPutSequence=zeros(Vres,Hres,fileCount);
+
+outPutFilePath=strcat('OutPut/',SourcePath);
+if ~exist(outPutFilePath) 
+    mkdir(outPutFilePath)         
+end 
+outPutFileWaveletPath=strcat(outPutFilePath,'/',motherWavelet,'_',num2str(CWTScale));
+if ~exist(outPutFileWaveletPath) 
+    mkdir(outPutFileWaveletPath)         
+end 
+
+%Read all image sequence into imageArray
+for u=1:fileCount
+  imageName=filePathList(u).name;
+  a=strcat(filePath,imageName);
+  imageArray(:,:,u)=rgb2gray(imread(a));
+end
+
+% ----------------------------------------------------------------
+
+% cwt
+V1=1;V2=Vres;
+deltaV=V2-V1;
+h = waitbar(0,'Please wait...');
+timeStart = tic;
+for V=V1:V2
+    for H=1:Hres
+        singlePixelSignal=[];
+        for frame=1:fileCount           
+            singlePixelSignal=[singlePixelSignal,imageArray(V,H,frame)];
+        end
+        coeffecients=cwt(singlePixelSignal,CWTScale,motherWavelet);
+        for frame=1:fileCount
+             maskOutPutSequence(V,H,frame)=uint16(abs(coeffecients(frame)));
+        end
+        str=[SourcePath,'-',motherWavelet,'-',num2str(CWTScale),'-请等待...',num2str((V-V1)/deltaV*100),'%'];
+        waitbar((V-V1)/deltaV,h,str);
+    end
+end
+timeOver = toc(timeStart);
+
+% save maskOutPutSequence data
+DataName = strcat(num2str(fileCount),'_MOPS_',motherWavelet,'_',num2str(CWTScale),'_',num2str(round(timeOver/60)),'mins','.mat');
+DataNamePath=strcat(outPutDataPath,DataName);
+save(DataNamePath,'maskOutPutSequence');
+
+% ----------------------------------------------------------------
+
+% show surf
+frame = floor(fileCount/2);
+% frame =4;
+surf(maskOutPutSequence(:,:,frame));
+shading interp;view(0,-90);title(strcat(motherWavelet,'-',num2str(frame)));
+
+% ----------------------------------------------------------------
+
+%Black
+clampBottom=75;
+%White
+clampHead=100;
+% save masked pic
+clampDelta=clampHead-clampBottom;
+alphaSequence16=zeros(Vres,Hres,fileCount);
+
+outPutFileWaveletAlphaPath=strcat(outPutFileWaveletPath,'/','Alpha_',num2str(clampBottom),'-',num2str(clampHead));
+if ~exist(outPutFileWaveletAlphaPath) 
+    mkdir(outPutFileWaveletAlphaPath)        
+end 
+outPutFileWaveletAlphaMaskPath=strcat(outPutFileWaveletAlphaPath,'/','Mask');
+if ~exist(outPutFileWaveletAlphaMaskPath) 
+    mkdir(outPutFileWaveletAlphaMaskPath)        
+end 
+for j=1:fileCount
+    for m=1:1080
+        for n=1:1920
+            if maskOutPutSequence(m,n,j)>clampHead
+                alphaSequence16(m,n,j)=0;
+            elseif maskOutPutSequence(m,n,j)<clampBottom
+                    alphaSequence16(m,n,j)=1;
+            else
+                alphaSequence16(m,n,j)=(clampHead-maskOutPutSequence(m,n,j))/clampDelta;
+            end
+        end
+    end
+    outPutFileName_temp=strcat(outPutFileWaveletAlphaMaskPath,'/outPutAlpha','.',int2str(j),'.tif');
+    imwrite(alphaSequence16(:,:,j),outPutFileName_temp);
+end
+
+% ----------------------------------------------------------------
+
+outPutFileWaveletMaskedPath=strcat(outPutFileWaveletAlphaPath,'/','Masked');
+if ~exist(outPutFileWaveletMaskedPath) 
+    mkdir(outPutFileWaveletMaskedPath)         
+end 
+% save masked pic
+for j=1:fileCount
+    imageName=strcat(filePath,filePathList(j).name);
+    I2=imread(imageName);
+    MaskedOutPutFileName=strcat(outPutFileWaveletMaskedPath,'/','MaskedPic.',int2str(j),'.png');
+    imwrite(I2,MaskedOutPutFileName,'Alpha',alphaSequence16(:,:,j));
+end
+
+% ----------------------------------------------------------------
+
+% create masked pic video
+outPutFileWaveletMaskedVideoPath=strcat(outPutFileWaveletAlphaPath,'/','MaskedVideo');
+if ~exist(outPutFileWaveletMaskedVideoPath) 
+    mkdir(outPutFileWaveletMaskedVideoPath)         
+end 
+VideoName = strcat(outPutFileWaveletMaskedVideoPath,'/','Video');
+v=VideoWriter(VideoName,'MPEG-4');
+v.FrameRate=24;
+open(v);
+for i=1:60
+OneMaskedPicName = strcat(outPutFileWaveletMaskedPath,'/','MaskedPic.',num2str(2*i-1),'.png');
+OneMaskedPic = imread(OneMaskedPicName);
+writeVideo(v,OneMaskedPic);
+end
+for i=1:60
+OneMaskedPicName = strcat(outPutFileWaveletMaskedPath,'/','MaskedPic.',num2str(2*i),'.png');
+OneMaskedPic = imread(OneMaskedPicName);
+writeVideo(v,OneMaskedPic);
+end
+close(v);
+
+% WSDataName = strcat(outPutDataPath,'ws_',DataName);
+% save(WSDataName);
